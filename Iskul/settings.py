c0bc2e5fa0cd3e452da48a7d59c8075ec4af0c21
Roopesh Acharya <36413342+roopeshach@users.noreply.github.com>
2021-06-05 01:03:18 +0545

    Update settings.py

--- Iskul/settings.py
+++ Iskul/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(rm1s&p#i0a&k3)35jv!x&yjx^gq$ns!4ifc2c=kdevwom-10j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
